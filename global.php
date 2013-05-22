919bb3198cf1199d740365af5bf05c23a9fd71c2
wbj <c@c.(none)>
2013-05-22 22:04:45 +0800

    修改链接数据库ip

--- global.php
+++ global.php
@@ -1,12 +1,13 @@
 <?php
-if($HTTP_HOST=="127.0.0.1:80"){
+if($HTTP_HOST=="127.0.0.1"){
    $db_user  =  "root";
    $db_password  =  "";
+   $db_server  =  "localhost";
 } else {
-   $db_user  =  "root";
+   $db_user  =  "root2";
    $db_password  =  "";
+   $db_server  =  "192.168.0.2";
 }
-$db_server  =  "localhost";
 $db_name   =  "bysj";
 $TABLE = "bysj_";
 $YEAR_C = "2013";
