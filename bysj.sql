d1b441ece2609b8c47e9e6de1b3153ec7570d1dc
wengbaijie <linux.c@foxmail.com>
2013-06-02 15:31:52 +0800

    添加数据库备份文件bysj.sql（艺术系新增表格部分）

---
+++ bysj.sql
@@ -0,0 +1,203 @@
+-- phpMyAdmin SQL Dump
+-- version 3.4.10.1deb1
+-- http://www.phpmyadmin.net
+--
+-- 主机: localhost
+-- 生成日期: 2013 年 06 月 02 日 15:29
+-- 服务器版本: 5.5.31
+-- PHP 版本: 5.3.10-1ubuntu3.6
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- 数据库: `bysj`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `art_instrument_student_select`
+--
+
+CREATE TABLE IF NOT EXISTS `art_instrument_student_select` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_number` varchar(16) CHARACTER SET gb2312 NOT NULL,
+  `first` tinyint(4) NOT NULL,
+  `second` tinyint(4) NOT NULL,
+  `third` tinyint(4) NOT NULL,
+  `finally` tinyint(4) NOT NULL,
+  `teacher` varchar(25) NOT NULL,
+  `year` varchar(4) NOT NULL,
+  `lock` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `art_major`
+--
+
+CREATE TABLE IF NOT EXISTS `art_major` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(25) CHARACTER SET gb2312 NOT NULL,
+  `icon` varchar(100) NOT NULL,
+  `detail` varchar(400) CHARACTER SET gb2312 NOT NULL,
+  `grade` tinyint(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `art_major_student_select`
+--
+
+CREATE TABLE IF NOT EXISTS `art_major_student_select` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_number` varchar(16) CHARACTER SET gb2312 NOT NULL,
+  `first` tinyint(4) NOT NULL,
+  `second` tinyint(4) NOT NULL,
+  `third` tinyint(4) NOT NULL,
+  `finally` tinyint(4) NOT NULL,
+  `teacher` varchar(25) NOT NULL,
+  `year` varchar(4) NOT NULL,
+  `lock` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `art_set_date`
+--
+
+CREATE TABLE IF NOT EXISTS `art_set_date` (
+  `id` tinyint(2) NOT NULL AUTO_INCREMENT,
+  `grade` tinyint(4) NOT NULL COMMENT '所属年级',
+  `topic_start` int(11) NOT NULL COMMENT '教师提交时间',
+  `topic_end` int(11) NOT NULL COMMENT '教师截止时间',
+  `student_start` int(11) NOT NULL COMMENT '学生选题时间',
+  `student_end` int(11) NOT NULL COMMENT '学生截止时间',
+  `teacher_start` int(11) NOT NULL COMMENT '选学生开始时间',
+  `teacher_end` int(11) NOT NULL COMMENT '选学生结束时间',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=gb2312 AUTO_INCREMENT=14 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `art_student_task`
+--
+
+CREATE TABLE IF NOT EXISTS `art_student_task` (
+  `sd_id` int(11) NOT NULL AUTO_INCREMENT,
+  `major_id` int(4) NOT NULL,
+  `classes` varchar(50) COLLATE gb2312_bin NOT NULL,
+  `task_id` varchar(2) COLLATE gb2312_bin NOT NULL,
+  `file_name` varchar(50) COLLATE gb2312_bin NOT NULL,
+  `status` int(1) NOT NULL,
+  `year` varchar(20) CHARACTER SET latin1 NOT NULL,
+  `teacher_id` varchar(50) CHARACTER SET latin1 NOT NULL,
+  `sd_grade` int(2) NOT NULL,
+  `student_num` varchar(16) CHARACTER SET gb2312 NOT NULL,
+  PRIMARY KEY (`sd_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=gb2312 COLLATE=gb2312_bin AUTO_INCREMENT=14 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `art_teacher_student`
+--
+
+CREATE TABLE IF NOT EXISTS `art_teacher_student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `major_id` int(11) NOT NULL,
+  `teacher_id` varchar(25) CHARACTER SET gb2312 NOT NULL,
+  `class` tinyint(4) NOT NULL,
+  `year` varchar(4) NOT NULL,
+  `value` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=53 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `art_teacher_task`
+--
+
+CREATE TABLE IF NOT EXISTS `art_teacher_task` (
+  `task_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) CHARACTER SET gb2312 COLLATE gb2312_bin NOT NULL,
+  `content` varchar(1000) CHARACTER SET gb2312 COLLATE gb2312_bin NOT NULL,
+  `start_time` varchar(100) CHARACTER SET gb2312 COLLATE gb2312_bin NOT NULL,
+  `end_time` varchar(100) CHARACTER SET gb2312 COLLATE gb2312_bin NOT NULL,
+  `teacher_task` varchar(100) NOT NULL,
+  `class` int(2) NOT NULL,
+  `year_task` varchar(20) NOT NULL,
+  `major_id` int(4) NOT NULL,
+  `grade` int(2) NOT NULL,
+  PRIMARY KEY (`task_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=40 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `art_title_sort`
+--
+
+CREATE TABLE IF NOT EXISTS `art_title_sort` (
+  `id` tinyint(3) NOT NULL AUTO_INCREMENT COMMENT '选题类别id',
+  `name` varchar(30) NOT NULL COMMENT '选题类别名称',
+  `open` tinyint(1) NOT NULL COMMENT '开启',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=gb2312 AUTO_INCREMENT=7 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `art_vocalmusic`
+--
+
+CREATE TABLE IF NOT EXISTS `art_vocalmusic` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `teacher_id` varchar(25) NOT NULL,
+  `vocalmusic` tinyint(4) NOT NULL,
+  `piano` tinyint(4) NOT NULL,
+  `year` varchar(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `art_vocalmusic_student_select`
+--
+
+CREATE TABLE IF NOT EXISTS `art_vocalmusic_student_select` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `student_number` varchar(25) CHARACTER SET gb2312 NOT NULL,
+  `vocalmusic_first` varchar(25) NOT NULL DEFAULT '0',
+  `vocalmusic_second` varchar(25) NOT NULL DEFAULT '0',
+  `vocalmusic_third` varchar(25) NOT NULL DEFAULT '0',
+  `vocalmusic_finally` varchar(25) NOT NULL DEFAULT '0',
+  `vocalmusic_lock` tinyint(4) NOT NULL,
+  `piano_first` varchar(25) NOT NULL DEFAULT '0',
+  `piano_second` varchar(25) NOT NULL DEFAULT '0',
+  `piano_third` varchar(25) NOT NULL DEFAULT '0',
+  `piano_finally` varchar(25) NOT NULL DEFAULT '0',
+  `piano_lock` tinyint(1) NOT NULL,
+  `year` varchar(4) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
