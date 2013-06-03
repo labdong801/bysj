2d9db8fde12ecc53f30b848261dfd56de2f0e83e
wengbaijie <linux.c@foxmail.com>
2013-06-03 12:24:06 +0800

    更新数据库备份文件bysj.sql（艺术系新增表格部分）

--- bysj.sql
+++ bysj.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2013 年 06 月 02 日 15:29
+-- 生成日期: 2013 年 06 月 03 日 12:20
 -- 服务器版本: 5.5.31
 -- PHP 版本: 5.3.10-1ubuntu3.6
 
@@ -37,7 +37,7 @@ CREATE TABLE IF NOT EXISTS `art_instrument_student_select` (
   `year` varchar(4) NOT NULL,
   `lock` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -53,6 +53,30 @@ CREATE TABLE IF NOT EXISTS `art_major` (
   `grade` tinyint(4) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+
+--
+-- 转存表中的数据 `art_major`
+--
+
+INSERT INTO `art_major` (`id`, `name`, `icon`, `detail`, `grade`) VALUES
+(1, '声乐', '\\images\\instrument\\shengyue.png', 'sadsafsafffffffffffffffffffasdasdasdasfasfasfasfasfasd', 3),
+(2, '钢琴', '\\images\\instrument\\gangqin.png', 'gangqingangqingangqin', 3),
+(3, '舞蹈', '\\images\\instrument\\wudao.png', 'wudaodasafasfasfasdasdasgfasda', 3),
+(4, '合唱指挥', '\\images\\instrument\\zhihui.png', 'zhihui', 3),
+(5, '二胡', '\\images\\instrument\\erhu.png', 'erhuerhuasdkakslcajkshjadlsad', 3),
+(6, '小提琴', '\\images\\instrument\\xiaotiqin.png', 'iasjdasjfklasjklajskldjasdads', 3),
+(7, '萨克斯/黑管', '\\images\\instrument\\shakesi.png', 'sadasfasfasfasfasfasdasdasd', 3),
+(8, '中国音乐理论', '\\images\\instrument\\zhongguo.png', '1241241252131231231241241241241241', 3),
+(9, '作曲技术理论', '\\images\\instrument\\zuoqu.png', 'sadasdasfasfasfasdas', 3),
+(10, '外国音乐理论', '\\images\\instrument\\waiguo.png', 'sajkasdhasjdkasdjlaksjdlasjdklasjdkasjdklasjkdajskdjalsdjas', 3),
+(11, '声乐', '', '', 2),
+(12, '钢琴', '', '', 2),
+(13, '小提琴', '\\images\\instrument\\xiaotiqin.png', '小提琴小提琴小提琴小提琴小提琴小提琴小提琴', 1),
+(14, '古筝', '\\images\\instrument\\guzheng.png', '古筝古筝古筝古筝古筝', 1),
+(15, '长笛', '\\images\\instrument\\changdi.png', '长笛长笛长笛长笛长笛', 1),
+(16, '萨克斯/黑管', '\\images\\instrument\\shakesi.png', '萨克斯/黑管萨克斯/黑管萨克斯/黑管萨克斯/黑管萨克斯/黑管萨克斯/黑管萨克斯/黑管萨克斯/黑管萨克斯/黑管萨克斯/黑管', 1),
+(17, '二胡', '\\images\\instrument\\erhu.png', '二胡二胡二胡二胡二胡二胡二胡二胡二胡二胡二胡二胡二胡二胡', 1),
+(18, '毕业设计', '', '', 4);
 
 -- --------------------------------------------------------
 
@@ -71,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `art_major_student_select` (
   `year` varchar(4) NOT NULL,
   `lock` tinyint(1) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -91,6 +115,16 @@ CREATE TABLE IF NOT EXISTS `art_set_date` (
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=gb2312 AUTO_INCREMENT=14 ;
 
+--
+-- 转存表中的数据 `art_set_date`
+--
+
+INSERT INTO `art_set_date` (`id`, `grade`, `topic_start`, `topic_end`, `student_start`, `student_end`, `teacher_start`, `teacher_end`) VALUES
+(10, 1, 0, 0, 1369180800, 1369958399, 1370044800, 1371081599),
+(11, 2, 0, 0, 1367798400, 1368748799, 1367539200, 1371081599),
+(12, 3, 0, 0, 1369094400, 1369958399, 1370131200, 1370476799),
+(13, 4, 1368576000, 1369958399, 1369267200, 1370476799, 1367452800, 1373241599);
+
 -- --------------------------------------------------------
 
 --
@@ -109,7 +143,7 @@ CREATE TABLE IF NOT EXISTS `art_student_task` (
   `sd_grade` int(2) NOT NULL,
   `student_num` varchar(16) CHARACTER SET gb2312 NOT NULL,
   PRIMARY KEY (`sd_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=gb2312 COLLATE=gb2312_bin AUTO_INCREMENT=14 ;
+) ENGINE=MyISAM DEFAULT CHARSET=gb2312 COLLATE=gb2312_bin AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -125,7 +159,7 @@ CREATE TABLE IF NOT EXISTS `art_teacher_student` (
   `year` varchar(4) NOT NULL,
   `value` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=53 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -145,7 +179,7 @@ CREATE TABLE IF NOT EXISTS `art_teacher_task` (
   `major_id` int(4) NOT NULL,
   `grade` int(2) NOT NULL,
   PRIMARY KEY (`task_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=40 ;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -159,6 +193,15 @@ CREATE TABLE IF NOT EXISTS `art_title_sort` (
   `open` tinyint(1) NOT NULL COMMENT '开启',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=gb2312 AUTO_INCREMENT=7 ;
+
+--
+-- 转存表中的数据 `art_title_sort`
+--
+
+INSERT INTO `art_title_sort` (`id`, `name`, `open`) VALUES
+(4, '课程教学', 1),
+(5, '科学研究', 1),
+(6, '测试', 1);
 
 -- --------------------------------------------------------
 
@@ -174,6 +217,19 @@ CREATE TABLE IF NOT EXISTS `art_vocalmusic` (
   `year` varchar(4) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=8 ;
+
+--
+-- 转存表中的数据 `art_vocalmusic`
+--
+
+INSERT INTO `art_vocalmusic` (`id`, `teacher_id`, `vocalmusic`, `piano`, `year`) VALUES
+(1, 'ty911', 0, 1, '2013'),
+(2, 'fj902', 0, 1, '2013'),
+(3, 'cxy910', 0, 1, '2013'),
+(4, 'dj901', 1, 0, '2013'),
+(5, 'cgl903', 1, 0, '2013'),
+(6, 'yxx905', 1, 0, '2013'),
+(7, 'lcm913', 1, 0, '2013');
 
 -- --------------------------------------------------------
 
@@ -196,7 +252,7 @@ CREATE TABLE IF NOT EXISTS `art_vocalmusic_student_select` (
   `piano_lock` tinyint(1) NOT NULL,
   `year` varchar(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=18 ;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
