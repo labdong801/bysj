63fc7d5a247031cea8371586a96dd19fb0a401b7
wengbaijie <linux.c@foxmail.com>
2013-06-03 11:55:28 +0800

    修改数据库链接文件

--- global.php
+++ global.php
@@ -1,12 +1,34 @@
 <?php
-if($HTTP_HOST=="127.0.0.1:80"){
-   $db_user  =  "root";
-   $db_password  =  "";
-} else {
+//if($HTTP_HOST=="127.0.0.1:80"){
+//   $db_user  =  "root";
+//   $db_password  =  "";
+//} else {
+//   $db_user  =  "root2";
+//   $db_password  =  "";
+//}
+if($HTTP_HOST=="127.0.0.1"){ 
    $db_user  =  "root2";
    $db_password  =  "";
+   $db_server  =  "192.168.0.2";
 }
-$db_server  =  "192.168.0.2";
+else if ($HTTP_HOST=="localhost")
+{
+	$db_user  =  "root2";
+	$db_password  =  "";
+	$db_server  =  "192.168.0.2";
+}
+else if ($HTTP_HOST=="192.168.0.2")
+{
+	$db_user  =  "root2";
+	$db_password  =  "";
+	$db_server  =  "192.168.0.2";
+} else {
+   $db_user  =  "root";
+   $db_password  =  "";
+   $db_server  =  "localhost";
+   
+}
+
 $db_name   =  "bysj";
 $TABLE = "bysj_";
 $YEAR_C = "2013";
